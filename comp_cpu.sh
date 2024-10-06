ef2d51e5917486d73e7945ea69919828dcf920bf
Tom Beardsley <t.beardsley@hotmail.co.uk>
2024-10-06 13:11:30 -0400

    Updated file structure

--- comp_cpu.sh
+++ comp_cpu.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #$ -cwd
 
-g++ -O2 -std=c++14 -lgsl -lgslcblas -lfftw3 fts_cpu.cc -o lfts-cpu
+mkdir build
+g++ -O2 -std=c++14 ./source/fts_cpu.cc -o ./build/lfts-cpu -lfftw3 -lgsl -lgslcblas -lm
