ac3ff0ae92b12c4ba0e6839d3d2fac6f276967f7
Tom Beardsley <t.beardsley@hotmail.co.uk>
2024-10-07 09:06:28 -0400

    Corrected src directory in comp.sh

--- comp_cpu.sh
+++ comp_cpu.sh
@@ -2,4 +2,4 @@
 #$ -cwd
 
 mkdir build
-g++ -O2 -std=c++14 ./source/fts_cpu.cc -o ./build/lfts-cpu -lfftw3 -lgsl -lgslcblas -lm
+g++ -O2 -std=c++14 ./src/fts_cpu.cc -o ./build/lfts-cpu -lfftw3 -lgsl -lgslcblas -lm
